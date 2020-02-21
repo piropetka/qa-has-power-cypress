d690a5571324dbc7d56ce818e41db58f67804116
piropetka <piropetka@gmail.com>
2020-02-21 20:54:06 +0100

    Ready to automate test

--- cypress/integration/qa-has-power/logIn.spec.js
+++ cypress/integration/qa-has-power/logIn.spec.js
@@ -1,6 +1,8 @@
 describe('Sign in with different methods', function() {
   beforeEach('navigate to home page', function() {
     cy.visit('/');
+  })
+  it('simple login', function() {
     cy.log('*Ready to automate!*')
   })
 })
