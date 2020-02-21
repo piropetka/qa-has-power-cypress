527a1cc60a6ef7769585221f7fefa5c83ccdbb3b
piropetka <piropetka@gmail.com>
2020-02-21 19:33:41 +0100

    Ready for workshops

--- cypress/integration/qa-has-power/logIn.spec.js
+++ cypress/integration/qa-has-power/logIn.spec.js
@@ -1,39 +1,6 @@
 describe('Sign in with different methods', function() {
   beforeEach('navigate to home page', function() {
     cy.visit('/');
-    cy.get('.se-pre-con').as('loader')
-    cy.get('@loader').should('not.be.visible')
-    cy.contains('Food&Drink')
-  })
-
-  it('simple login', function() {
-    cy.get('[href="/login"]').click()
-    cy.get('@loader').should('not.be.visible')
-    cy.url().should('include', '.com/login')
-    cy.get('#email').type('cypress123@automation.com')
-    cy.get('#password').type('AdamKowalski123!')
-    cy.get('form').submit()
-    cy.get('@loader').should('not.be.visible')
-    cy.url().should('include', '.com/dashboard')
-    cy.contains('Zostałeś zalogowany')
-  })
-
-  it('login using fixtures', function() {
-    cy.get('[href="/login"]').click()
-    cy.get('@loader').should('not.be.visible')
-    cy.url().should('include', '.com/login')
-    cy.fixture('defaultUser').then((user) => {
-      cy.get('#email').type(user.email)
-      cy.get('#password').type(user.password + '{enter}')
-    })
-    cy.get('@loader').should('not.be.visible')
-    cy.url().should('include', '.com/dashboard')
-    cy.contains('Zostałeś zalogowany')
-  })
-
-  it('login using fixtures and custom command', function() {
-    cy.fixture('defaultUser').then((user) => {
-      cy.login(user, 'Zostałeś zalogowany')
-    })
+    cy.log('*Ready to automate!*')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-Cypress.Commands.add("login", (user, message) => {
-  return cy.request({
-    method: "POST",
-    url: "/login",
-    form: true,
-    body: user
-  }).then((response) => {
-    expect(response.body).to.contain(message)
-  })
-});
-
 Cypress.Commands.add("test", () => {
   cy.log('Function works well')
 });
